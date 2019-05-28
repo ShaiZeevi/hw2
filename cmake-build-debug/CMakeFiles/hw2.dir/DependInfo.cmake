06a54fc3031a9bcd2d5e68dd1ebe386ba3c49eac
ShaiZeevi <shai.zeevi@campus.technion.ac.il>
2019-05-28 16:32:58 +0300

    solved question A

---
+++ cmake-build-debug/CMakeFiles/hw2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/shai.zeevi/CLionProjects/Competitive Programming/hw2/uva_1254.cpp" "C:/Users/shai.zeevi/CLionProjects/Competitive Programming/hw2/cmake-build-debug/CMakeFiles/hw2.dir/uva_1254.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- uva_11362.cpp
+++ uva_11362.cpp
@@ -1,6 +1,77 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+typedef vector<int> vi;
+
+//string SA_str; // the input string, up to 100K characters
+//int m; // the length of input string
+//vi RA, tempRA; // rank array and temporary rank array
+//vi SA, tempSA; // suffix array and temporary suffix array
+//vi c; // for counting/radix sort
+//
+//void countingSort(int k) { // O(m)
+//    int n = SA_str.length()-1;
+//    int i, sum, maxi = max(300, n); // up to 255 ASCII chars or length of m
+//    c.assign(maxi,0);
+//    for (i = 0; i < n; i++) // count the frequency of each integer rank
+//        c[i + k < n ? RA[i + k] : 0]++;
+//    for (i = sum = 0; i < maxi; i++) {
+//        int t = c[i]; c[i] = sum; sum += t; }
+//    for (i = 0; i < n; i++) // shuffle the suffix array if necessary
+//        tempSA[c[SA[i]+k < n ? RA[SA[i]+k] : 0]++] = SA[i];
+//    for (i = 0; i < n; i++) // update the suffix array SA
+//        SA[i] = tempSA[i];
+//}
+//
+//void constructSA() { // this version can go up to 100000 characters
+//    int n = SA_str.length()-1;
+//    RA.assign(n,0);
+//    tempRA.assign(n,0);
+//    SA.assign(n,0);
+//    tempSA.assign(n,0);
+//    int i, k, r;
+//    for (i = 0; i < n; i++) RA[i] = SA_str[i]; // initial rankings
+//    for (i = 0; i < n; i++) SA[i] = i; // initial SA: {0, 1, 2, ..., m-1}
+//    for (k = 1; k < n; k <<= 1) { // repeat sorting process log m times
+//        countingSort(k); // actually radix sort: sort based on the second item
+//        countingSort(0); // then (stable) sort based on the first item
+//        tempRA[SA[0]] = r = 0; // re-ranking; start from rank r = 0
+//        for (i = 1; i < n; i++) // compare adjacent suffixes
+//            tempRA[SA[i]] = // if same pair => same rank r; otherwise, increase r
+//                    (RA[SA[i]] == RA[SA[i-1]] && RA[SA[i]+k] == RA[SA[i-1]+k]) ? r : ++r;
+//        for (i = 0; i < n; i++) // update the rank array RA
+//            RA[i] = tempRA[i];
+//        if (RA[SA[n-1]] == n-1) break; // nice optimization trick
+//    }
+//}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; ++t) {
+        int n;
+        scanf("%d\n", &n);
+        vector<string> phoneNumbers(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> phoneNumbers[i];
+        }
+
+        sort(phoneNumbers.begin(), phoneNumbers.end());
+
+        bool consistent = true;
+        for (int j = 0; j < n - 1; ++j) {
+            //compare j with j + 1
+            if (phoneNumbers[j] == phoneNumbers[j + 1].substr(0, phoneNumbers[j].length())) {
+                cout << "NO" << endl;
+                consistent = false;
+                break;
+            }
+        }
+
+        if (consistent) cout << "YES" << endl;
+    }
     return 0;
 }
